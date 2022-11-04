56739971345a2cc8986a3f68ca7a3c708b05d95a
Sally Steuterman <sally.steuterman@gmail.com>
2022-11-04 15:43:35 -0500

    Initial html layout of homepage

--- src/App.js
+++ src/App.js
@@ -5,18 +5,30 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Orbit Report</h1>
+        <table>
+          <tr>
+            <th>Name</th>
+            <th>Type</th>
+            <th>Operational</th>
+            <th>Orbit Type</th>
+            <th>Launch Date</th>
+          </tr>
+          <tr>
+            <td>Sirius 1</td>
+            <td>Communication</td>
+            <td>true</td>
+            <td>LOW</td>
+            <td>2007-11-17</td>
+          </tr>
+        </table>
+        {/* Search goes here */}
+
+        <h3>Satellite Count:</h3>
+        <div>
+          Total:
+          <span>1</span>
+        </div>
       </header>
     </div>
   );
